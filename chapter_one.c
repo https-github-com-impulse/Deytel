a91ca25546fe4036bb093c661d9f0b35f770fb70
Impulsepro <impulse.devlop@gmail.com>
2023-05-16 09:49:32 -0200

    starting learn c with Deytel book

---
+++ chapter_one.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+  int  main() {
+    /* code
+    printf("%d\n",getOper() );
+    printf("%d",t18() );*/
+    printf("%d",t19() );
+    return 0;
+  }
+
+/*int getOper(void){
+    int a,b;
+    scanf("%d %d",&a,&b );
+    printf("Сумма %d, разность %d, произведение %d, частное %d /n",a+b,
+    a-b,a*b,a/b );
+}*/
+/*int t18(void) {
+    int a,b;
+    printf("Введите два целых числа\n" );
+    scanf("%d %d",&a,&b );
+    if(a>b){
+      printf("%d is larger than %d \n",a,b );
+    }else if (a==b) {
+      printf("%d equal %d \n",a,b );
+    }else{
+      printf(" %d no equal and smallest than %d \n",a,b);
+    }
+}*/
+
+int t19(void) {
+  int a,b,c,sum,mult,larger,smallest;
+  printf("%d Input three different integers: ");
+  scanf("%d %d %d",&a,&b,&c);
+  printf("Sum is %d \nAverage is %d\nProduct is %d\n",a+b+c,(a+b+c)/3,(a*b*c));
+  if(a<b && a<c){
+      printf("Smallest is %d \n",a);
+  }else if (b<a && b<c) {
+      printf("Smallest is %d \n",b);
+  }else{
+      printf("Smallest is %d \n",c);
+  }
+  if(a>b && a>c){
+      printf("Largest is %d \n",a);
+  }else if (b>a && b>c) {
+      printf("Largest is %d \n",b);
+  }else{
+      printf("Largest is %d \n",c);
+  }
+}

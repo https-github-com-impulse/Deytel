2e57acaa7eed28e2ea23235c6fcd9943ca905704
Impulsepro <impulse.devlop@gmail.com>
2023-05-22 07:44:25 -0200

    shit chapter its diff(

---
+++ Chapter_Four/Cp4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  printf("%d",task2());
+  return 0;
+}
+
+      /*int task(void){
+          int sum;
+          for(int i = 1; i<=99;i++){
+            if(i%2!=0){
+              sum+=i;
+            }
+          }
+          printf("Сумма нечетных целых  = %d \n",sum);
+      }*/
+        int task2(){
+          printf("%10.2f\n",pow(2.5,3));
+      }

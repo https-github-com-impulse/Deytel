989fc869200d0b0515d2bba185f750ab29a3d887
Impulsepro <impulse.devlop@gmail.com>
2023-05-25 11:57:02 -0200

    more printf plz

--- MoneyManagement/mm.c
+++ MoneyManagement/mm.c
@@ -2,8 +2,8 @@
 #include <math.h>
 
     int main(){
-            printf("%d",calculateJKH());
-
+            //printf("%d",calculateJKH());
+            printf("%d",checkJKH() );
     }
 
 
@@ -17,8 +17,8 @@
     }*/
 
  /* Написать функцию - метод для сверки  показаний ЖКХ показаний * тариф (в будущем занос в DB)*/
- 
-    int calculateJKH(){
+
+        /*int calculateJKH(){
 
         float watherAcc,repairAcc,tsjAcc,
         electroAcc,domophoneAcc,gasAcc,sumJKH;
@@ -30,7 +30,7 @@
          scanf("%f",&repairAcc);
          printf("%s","ТСЖ: ");
          scanf("%f",&tsjAcc);
-         printf("%s","Эллектричество + Мусор : ");
+         printf("%s","Эллектричество + Мусор: ");
          scanf("%f",&electroAcc);
          printf("%s","Домофон: " );
          scanf("%f",&domophoneAcc);
@@ -42,4 +42,43 @@
 
          printf("%s%.2f рублей.\n","Сумма ЖКХ в этом месяце составила:  ",sumJKH);
 
-    }
+}*/ //++
+
+        int checkJKH(){
+
+            const float staticElectroTariff = 4.01;//эл тариф
+            const float staticWatherTariff = 40.82; //мусор тариф
+            const float staticGasTariff = 1488;
+
+            const float currentElectroTariff;
+            const float currentWatherTariff;
+            const float currentGasTariff;
+
+            float lastMonthElectroRate,lastMonthGasRate,
+            lastMonthWatherRate,currentElectroRate,currentGasRate,currentWatherRate,promResElectro,promResGas,promResWather;
+
+                printf("%s","Введите текущие показания счетчиков учета:\nЭлектроэнергия: ");
+                    scanf("%f",&currentElectroRate);
+                printf("%s\n","Газ: ");
+                    scanf("%f",&currentGasRate);
+                printf("%s","Вода: ");
+                    scanf("%f",&currentWatherRate);
+
+                printf("%s","Введите показания прошлого месяца :\nЭлектроэнергия: ");
+                    scanf("%f",&lastMonthElectroRate);
+                printf("%s","Газ: ");
+                    scanf("%f",&lastMonthGasRate);
+                printf("%s","Вода: ");
+                    scanf("%f",&lastMonthWatherRate);
+
+                printf("%s","Введите тариф на электроэнергию: ");
+                    scanf("%f",&currentElectroTariff);
+                printf("%s","Введите тариф на газ: ");
+                    scanf("%f",&currentGasTariff);
+                printf("%s","Введите тариф на водоснабжение: ");
+                    scanf("%f",&currentWatherTariff);
+
+                promResElectro =
+
+
+        }

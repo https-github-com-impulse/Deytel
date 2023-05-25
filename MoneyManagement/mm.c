629dabfa8760e8895f9c1225338760fd4466d9ee
Impulsepro <impulse.devlop@gmail.com>
2023-05-25 09:39:55 -0200

    need more changes

--- MoneyManagement/mm.c
+++ MoneyManagement/mm.c
@@ -2,34 +2,44 @@
 #include <math.h>
 
     int main(){
-            switch (/* expression */) {
-                case /* value */:
-            }
+            printf("%d",calculateJKH());
 
     }
 
 
 
 /*программа должна расчитывать  и распределять зарплату для  распостранения равномерных затрат на месяц*/
-    int calculate(){
+    /*int calculate(){
         int monthMoney
         printf("%s\n","Введите вашу зарплату: " );
         scanf("%f \n",&monthMoney);
 
-    }
+    }*/
 
-
+ /* Написать функцию - метод для сверки  показаний ЖКХ показаний * тариф (в будущем занос в DB)*/
+ 
     int calculateJKH(){
+
         float watherAcc,repairAcc,tsjAcc,
         electroAcc,domophoneAcc,gasAcc,sumJKH;
-         printf("%s\n","Введите все данные комуннальных платежек по очереди:\n Вода:\nКомунальный ремонт:\n ТСЖ:\nЭллектричество + Мусор:\nДомофон:\nГаз:\n" );
 
-         scanf("%s\n",&watherAcc,&repairAcc,&tsjAcc,
-        &electroAcc,&domophoneAcc,&gasAcc);
+         printf("%s\n","Введите все данные комуннальных платежек по очереди: ");
+         printf("%s","Вода: ");
+         scanf("%f",&watherAcc);
+         printf("%s","Ком.Ремонт: ");
+         scanf("%f",&repairAcc);
+         printf("%s","ТСЖ: ");
+         scanf("%f",&tsjAcc);
+         printf("%s","Эллектричество + Мусор : ");
+         scanf("%f",&electroAcc);
+         printf("%s","Домофон: " );
+         scanf("%f",&domophoneAcc);
+         printf("%s","Газ: " );
+         scanf("%f",&gasAcc);
 
-        sumJKH = watherAcc+repairAcc+tsjAcc+
+          sumJKH = watherAcc+repairAcc+tsjAcc+
         electroAcc+domophoneAcc+gasAcc;
 
-        printf("%s%.2f\n","Сумма ЖКХ в этом месяце составила: %f",sumJKH);
-            
+         printf("%s%.2f рублей.\n","Сумма ЖКХ в этом месяце составила:  ",sumJKH);
+
     }

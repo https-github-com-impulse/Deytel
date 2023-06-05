0a901276d6a0e567f4e49a629ddb6f6e80725977
Impulsepro <impulse.devlop@gmail.com>
2023-06-05 08:38:03 -0200

    hypotenuse task! done gg

---
+++ Chapter_Five(Functions)/Ex1.1/1.1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+    double hypotenuse(double,double);
+
+    int main(){
+        double sideA,sideB;
+        for(int i = 1;i<=3;i++){
+            printf("%s\n","Введите стороны треугольника: ");
+            scanf("%lf%lf",&sideA,&sideB);
+
+            printf("Для треугольника %d со сторонами A = %g B = %g гипотенуза = %g\n",i,sideA,sideB,hypotenuse(sideA,sideB));
+        }
+
+}
+
+    double hypotenuse (double sideA,double sideB){
+        double hypo = (sideA*sideA)+(sideB*sideB);
+        return hypo;
+}
